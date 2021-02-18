108574c79141936617b4a4fb4d40ad3198283f27
themikaelson <34260590+themikaelson@users.noreply.github.com>
2021-02-17 21:04:16 -0500

    fix conflix indexjs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-    <App />,
-=======
+import "tachyons";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> cd6ddbc... Initialize project using Create React App
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
